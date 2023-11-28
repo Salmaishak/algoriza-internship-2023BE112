deab229d3b9de10e12fed694ce19eb642b7ed377
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-11-29 00:52:18 +0200

    fixing bugs

---
+++ Vezeeta.Core/Vezeeta.Infrastructure/Migrations/20231128225144_updatevalidation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vezeeta.Infrastructure.Migrations
+{
+    public partial class updatevalidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
