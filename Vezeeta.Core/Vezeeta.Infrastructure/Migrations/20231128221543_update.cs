75d31534c3110bea5a40c75b1c273f0944f11c14
Salma Magdy Ishak <salma.m.ishakk@gmail.com>
2023-11-29 00:16:14 +0200

    check up for change migration

---
+++ Vezeeta.Core/Vezeeta.Infrastructure/Migrations/20231128221543_update.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vezeeta.Infrastructure.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
